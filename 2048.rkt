e1e40642e3b482c480fc6a5884a1d2fe1b6f600b
Ethan Yang <ethan.y@outlook.com>
2014-12-31 18:27:47 -0800

    get-a-piece

--- 2048.rkt
+++ 2048.rkt
@@ -4,3 +4,13 @@
   (make-list n( make-list n 0)))
 
 (make-board 4)
+
+(define PIECE_DIST '(2 2 2 2 2 2 2 2 2 4))
+(define (choice l)
+  (if (list? l) (list-ref l (random (length l)))
+    (vector-ref l (random (vector-length l)))))
+
+(define (get-a-piece)
+  (choice PIECE_DIST))
+
+(get-a-piece)
